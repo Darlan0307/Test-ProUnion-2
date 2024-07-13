2922ca3833a1d6c529737d68f822caadb9fac0e8
Darlan0307 <darlanchagas2020@gmail.com>
2024-07-13 10:07:19 -0300

    feat: criando função para proteger as rotas de pessoas não autorizadas

---
+++ backend/src/middleware/AuthMiddleware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import jsonwebtoken from "jsonwebtoken";
+
+type TokenPayload = {
+  id: string;
+  iat: number;
+  exp: number;
+};
+
+export function AuthMiddleWares(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { authorization } = req.headers;
+  // Verificando se o token existe
+  if (!authorization) {
+    return res.status(401).json({ message: "Nenhum token enviado" });
+  }
+
+  // Extraindo o token
+  const [, token] = authorization.split(" ");
+
+  try {
+    // Verificando se o token é válido
+    const secret = process.env.SECRET;
+    const decoded = jsonwebtoken.verify(token, `${secret}`);
+    const { id } = decoded as TokenPayload;
+    req.userId = id;
+    // passando para o próximo middleware
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token invalido" });
+  }
+}
