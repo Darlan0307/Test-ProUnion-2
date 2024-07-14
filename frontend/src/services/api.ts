0a656e0796e1cd79fdaebc3d8ff28188eb1a6727
Darlan0307 <darlanchagas2020@gmail.com>
2024-07-14 19:54:12 -0300

    fix: resolvendo conflito na comunicação entre os containers do docker

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Criando a instância do axios com a baseURL e headers definidos
 export const api = axios.create({
-  baseURL: "http://localhost:5002",
+  baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
